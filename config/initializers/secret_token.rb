adde2185d9004a343527fbe43d95b06f60c8c0db
Dave Ferris <davidmferris@gmail.com>
2014-01-20 17:30:25 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sumeau::Application.config.secret_token = 'aff5994b5492cc1f6e66f629d0b02da2f053dedfc761bcbf0d91c63d581bce766215735c74a837eca32ce46391afc6cdbd4b7b830ee78ebdb4857246d5930641'
